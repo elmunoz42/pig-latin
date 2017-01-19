d609169a344b3112eede7f2713b1e12b9d62519a
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-01-18 22:05:49 -0800

    add improved business logic.

--- js/alternatescripts.js
+++ js/alternatescripts.js
@@ -12,28 +12,30 @@ var anslatortray =function(userInput){
 
   wordArray = userInput.split("");
 
+  /// for words starting with vowels
   if (wordArray[0] === "a" | wordArray[0] ==="e" | wordArray[0] === "i" | wordArray[0] === "o" | wordArray[0] === "u") {
     pigArray = wordArray.push("ay");
   }
+  /// for words starting with consonants
   else {
-    do { var i = 0
+    while (
+      thereIsVowel === false
+    ) do { var i = 0
       if (wordArray[i] === "a" | wordArray[i] ==="e" | wordArray[i] === "i" | wordArray[i] === "o" |      wordArray[i] === "u") {
+        consonant = consonant.push(wordArray[i]); /// every time it's not a vowel add to our array of consonants at the beginning
         thereIsVowel = true;
-        pigArray = wordArray.push("ay")
       }
       else {
         i += 1;
       }
     }
-    while (
-      thereIsVowel === false
-    )
-    consonant = wordArray.slice(0,i-1); /// slice from index 0 through where the first vowel was detected
-    consonantString = consonant.join(""); /// make a string out of consonant
-    consonantLength = i; /// consonant length is equal to the index where the vowel was detected
+    // consonant = wordArray.slice(0,i-1); /// slice from index 0 through where the first vowel was detected
+    //consonantLength = i; /// consonant length is equal to the index where the vowel was detected
+    consonantString = consonant.join(""); /// make a string out of consonant array
     remainder = wordArray.splice(0,i-1); /// splice from index 0 through where the first vowel was detected
     remainderLength = remainder.length;  ///  take the remaining length of the array
     pigArray = wordArray.splice(remaniderLength,0,consonantString);    /// splice from remainderLength and add consonant in string form.
+    pigArray = wordArray.push("ay")
   }
 
   // else if (wordArray[j] === "q" && wordArray[j+1] === "u") {
