35addc65b9125821e5249acfcd764815095c35a7
Brynna McCarter and Carlos Munoz Kampff <carlos@>
2017-01-18 16:40:46 -0800

    add for loop (known errors)

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,51 @@
 // ////////BE////////
 var userInput;
-var wordArray = [];
-var firstVowel;
+var wordArray;
+var firstIsVowel;
+var vowelIndex = [];
+var wordLength;
+var yetAnotherVariable = [];
+var vowels = ["a","e","i","o","u"];
 var anslatortray =function(userInput){
-  // var userInput = prompt("userInput");
-  var result = "atcay";
   wordArray = userInput.split("");
+  // var userInput = prompt("userInput");
+  var result;
+  firstIsVowel = false;
+  for (var i=0; firstIsVowel === false; i += 1) {
+    if (wordArray[0] === vowels[i]) {
 
-  firstVowel = wordArray.indexOf("a");
-  if (firstVowel === 0) {
-    result = userInput + "ay";
-  } else {
-    var result = userInput;
+      firstIsVowel = true;
+      return wordArray.toString() + "ay";
+    } else if (wordArray[0] != vowels[i]){
+      alert("doom!!!!");
+      wordLength = wordArray.length;
+      alert ("wordLength" + wordLength);
+      // yetAnotherVariable =
+      wordArray = wordArray.push("c");
+      // wordArray = wordArray.splice(0,1);
+      alert ("wordArray " + wordArray);
+      wordArray = wordArray.toString();
+      return wordArray + "ay";
+
+    }
   }
-    return result;
-};
+
+}
+  // vowels.forEach(function(vowel){
+    // wordArray = userInput.split("");
+  //   console.log(wordArray);
+  //   firstVowel = wordArray.indexOf(vowel);
+  //   console.log(firstVowel);
+  //   // return firstVowel;
+  // });
+//
+//   if (firstVowel === 0) {
+//     result = userInput + "ay";
+//   } else {
+//     var result = userInput;
+//   }
+//     return result;
+// };
 
 // // var cat=["c","a","t"]
 // // cat.indexOf("a");
@@ -25,12 +56,13 @@ $(document).ready(function(){
   $("#user-input").submit(function(event){
     // var userInput = prompt("userInput");
     var userInput = $("input#text-input").val();
-    alert("userInput: " + userInput);//debugging
     var userOutput = anslatortray(userInput);
+    // userOutput = userOutput.join("");
     $("p#text-output").text(userOutput);
-    alert("userOutput: " + userOutput);//debugging
-    alert("wordArray: " + wordArray);//debugging
-    alert("firstVowel: " + firstVowel);//debugging
+    // alert("userInput: " + userInput);//debugging
+    // alert("userOutput: " + userOutput);//debugging
+    // alert("wordArray: " + wordArray);//debugging
+    // alert("firstVowel: " + firstVowel);//debugging
     event.preventDefault();
   });
 
